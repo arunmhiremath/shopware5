09002f88484a875b5660567516a52c8276b09a49
arunmhiremath <57758641+arunmhiremath@users.noreply.github.com>
2021-04-23 12:00:15 +0200

    Update Detail.php

--- engine/Shopware/Controllers/Frontend/Detail.php
+++ engine/Shopware/Controllers/Frontend/Detail.php
@@ -141,15 +141,25 @@ class Shopware_Controllers_Frontend_Detail extends Enlight_Controller_Action
             $categoryInfo = null;
         }
 
+        //$deliverday = $product['lieferung'];
+        //dd($deliverday);
+        //$today = date_create('now')->format("Y-m-d");
+        //dd($today);
+
+        //$day = date_diff($deliverday, $today);
+        //dd($day);
+
         // SW-3493 sArticle->getArticleById and sBasket->sGetGetBasket differ in camelcase
         $product['sReleaseDate'] = $product['sReleasedate'];
 
-        $day = (date_diff($product['lieferung']) (date_create('now')));
+
+
+
 
         $this->View()->assign('sBreadcrumb', $breadcrumb);
         $this->View()->assign('sCategoryInfo', $categoryInfo);
         $this->View()->assign('sArticle', $product);
-        $this->View()->assign('sDay', $day);
+        //$this->View()->assign('sDay', $day);
         $this->View()->assign('rand', Random::getAlphanumericString(32));
 
 
